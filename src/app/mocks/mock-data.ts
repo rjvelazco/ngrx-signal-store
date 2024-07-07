33af4e465201531b53a6ecf124418384f634e489
Rafael Velazco <rjvelazco21@gmail.com>
2024-07-07 16:01:13 -0400

    chore: code cleanup

---
+++ src/app/mocks/mock-data.ts
@@ -0,0 +1,54 @@
+import { Todo } from "../models/todo.models";
+
+export const TODOS: Todo[] = [
+    {
+        id: '1',
+        title: 'Learn Angular',
+        completed: false
+    },
+    {
+        id: '2',
+        title: 'Learn TypeScript',
+        completed: true
+    },
+    {
+        id: '3',
+        title: 'Learn RxJS',
+        completed: false
+    },
+    {
+        id: '4',
+        title: 'Learn NgRx',
+        completed: false
+    },
+    {
+        id: '5',
+        title: 'Learn Angular Material',
+        completed: true
+    },
+    {
+        id: '6',
+        title: 'Learn Angular Flex-Layout',
+        completed: false
+    },
+    {
+        id: '7',
+        title: 'Learn Angular Animations',
+        completed: false
+    },
+    {
+        id: '8',
+        title: 'Learn Angular PWA',
+        completed: true
+    },
+    {
+        id: '9',
+        title: 'Learn Angular Universal',
+        completed: false
+    },
+    {
+        id: '10',
+        title: 'Learn Angular SEO',
+        completed: false
+    }
+];
